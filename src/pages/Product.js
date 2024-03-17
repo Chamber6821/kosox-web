d5c75c2b81e6602f7743bc8c9a6859648464e0ca
Chamber6821 <vania.osinin@yandex.ru>
2024-03-17 22:11:12 +0300

    feat: translate product.html to Product.js

---
+++ src/pages/Product.js
@@ -0,0 +1,357 @@
+export default function () {
+  return (
+    <>
+      <header className='header'>
+        <nav className='navbar'>
+          <div className='navbar_logo'>
+            <a href='/'>
+              <div className='logo'>
+                <strong>KOSOX</strong>
+              </div>
+            </a>
+          </div>
+          <div className='navbar_menu'>
+            <a href='mailto:'>zakaz@mgb-bearings.ru</a>
+          </div>
+          <div className='navbar_icon'>
+            <a href='#'>
+              <img
+                src='https://mgb-bearings.ru/wp-content/themes/astra-child/assets/image/whatsapp.svg'
+                alt=''
+              />
+            </a>
+            <a href='#'>
+              <img
+                src='https://mgb-bearings.ru/wp-content/themes/astra-child/assets/image/viber.svg'
+                alt=''
+              />
+            </a>
+            <a href='#'>
+              <img
+                src='https://mgb-bearings.ru/wp-content/themes/astra-child/assets/image/telegram.svg'
+                alt=''
+              />
+            </a>
+          </div>
+        </nav>
+        <nav className='navbar_top'>
+          <ul className='navbar_top_menu'>
+            <li className='navbar_top_menu_li'>
+              <a href='/'>Главная</a>
+            </li>
+            <li className='navbar_top_menu_li'>
+              <a href='./aboutcompany.html'>О компании</a>
+            </li>
+            <li className='navbar_top_menu_li'>
+              <a href='./shippingandpayment.html'>Доставка и Оплата</a>
+            </li>
+            <li className='navbar_top_menu_li'>
+              <a href='./manufacturers.html'>Производители</a>
+            </li>
+            <li className='navbar_top_menu_li'>
+              <a href='./faq.html'>FAQ</a>
+            </li>
+            <li className='navbar_top_menu_li'>
+              <a href='./contact.html'>Контакты</a>
+            </li>
+          </ul>
+        </nav>
+        <div className='navbar_phone'>
+          <div className='navbar_phone_logo'>
+            <a href='/'>
+              <div className='logo'>
+                <strong>KOSOX</strong>
+              </div>
+            </a>
+          </div>
+          <div className='navbar_phone_menu'>
+            <i onclick='menuop()' className='bx bx-menu-alt-right' />
+          </div>
+          <div className='navbar_phone_right_menu'>
+            <i onclick='menucl()' className='bx bxs-right-arrow-alt' />
+            <ul className='navbar_top_menu'>
+              <li className='navbar_top_menu_li'>
+                <a href='/'>Главная</a>
+              </li>
+              <li className='navbar_top_menu_li'>
+                <a href='./aboutcompany.html'>О компании</a>
+              </li>
+              <li className='navbar_top_menu_li'>
+                <a href='./shippingandpayment.html'>Доставка и Оплата</a>
+              </li>
+              <li className='navbar_top_menu_li'>
+                <a href='./manufacturers.html'>Производители</a>
+              </li>
+              <li className='navbar_top_menu_li'>
+                <a href='./faq.html'>FAQ</a>
+              </li>
+              <li className='navbar_top_menu_li'>
+                <a href='./contact.html'>Контакты</a>
+              </li>
+            </ul>
+            <div className='navbar_phone_right_menu_mail'>
+              <a href='mailto:'>zakaz@mgb-bearings.ru</a>
+            </div>
+            <div className='navbar_icon'>
+              <a href='#'>
+                <img
+                  src='https://mgb-bearings.ru/wp-content/themes/astra-child/assets/image/whatsapp.svg'
+                  alt=''
+                />
+              </a>
+              <a href='#'>
+                <img
+                  src='https://mgb-bearings.ru/wp-content/themes/astra-child/assets/image/viber.svg'
+                  alt=''
+                />
+              </a>
+              <a href='#'>
+                <img
+                  src='https://mgb-bearings.ru/wp-content/themes/astra-child/assets/image/telegram.svg'
+                  alt=''
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <nav className='navbar_down'>
+          <a href='' className='red_border'>
+            КАТАЛОГ ТОВАРОВ
+          </a>
+          <div className='navbar_down_inp'>
+            <input placeholder='Введите наименование' type='text' />
+            <img src='./img/find.svg' alt='' />
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className='product_page content'>
+          <div className='product_page_about '>
+            <div className='product_page_about_img'>
+              <div className='product_page_about_img_bg'></div>
+            </div>
+            <div className='product_page_about_title'>
+              <h1>Ареометр для антифриза FFH</h1>
+              <div className='product_page_about_title_fenom'>
+                <h3>Производитель :</h3>
+                <img src='./img/logo-150x67-11.png' alt='' />
+              </div>
+              <a href=''>Заказать</a>
+            </div>
+          </div>
+        </div>
+        <div className='product_page_description'>
+          <div className='product_page_description_menu'>
+            <div
+              onclick='productdesc()'
+              className='product_page_description_menu_title'
+            >
+              <img
+                className='img_texteditor'
+                src='./img/texteditor.svg'
+                alt=''
+              />
+              <h2 className='h2_texteditor'>Описание</h2>
+            </div>
+            <div
+              onclick='productharakt()'
+              className='product_page_description_menu_title2'
+            >
+              <img className='img_searchbox' src='./img/searchbox.svg' alt='' />
+              <h2 className='h2_searchbox'>Характеристики</h2>
+            </div>
+          </div>
+          <div className='produce_page_description_text content'>
+            <p>
+              В нашей компании Вы можете купить MGB — Бесшпоночная зажимная
+              муфта (втулка) BK71 150×200 оптом и в розницу, а так же другие
+              комплектующим, представленные в нашем каталоге. Тип KLDB, RCK71,
+              KTR200 относится к быстрозажимным типам. Их производят для
+              установки на вал электродвигателя особой плотности. Соединение при
+              этом безлюфтовое. Не требует применения особых инструментов.
+              Монтаж проводится за несколько минут. Простота использования
+              заключается в том, что втулки не образуют зазоров из-за отсутствия
+              шлиц. Основным элементом служат разрезные конусы. При стягивании
+              крепежных элементов они плотно стягиваются. Во время данного
+              действия, происходит радиальная деформация и внутреннее кольцо
+              уменьшается. Изделие создается из углеродистой стали, что
+              обеспечивает ему высокую прочность и долговечность. Втулки
+              упрощают техническое обслуживание всего механизма.
+            </p>
+            <a href=''>Заказать данный товар</a>
+          </div>
+          <div className='produce_page_description_text2'>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Длина L, мм.</td>
+                  <td>405</td>
+                </tr>
+                <tr>
+                  <td>Цена деления шкалы, кг/м3</td>
+                  <td>1</td>
+                </tr>
+                <tr>
+                  <td>Производитель</td>
+                  <td>MGB</td>
+                </tr>
+              </tbody>
+            </table>
+            <a href=''>Заказать данный товар</a>
+          </div>
+        </div>
+      </main>
+      <footer className='allFooter'>
+        <div className='allFooter_div'>
+          <div className='allFooter_div_block_one'>
+            <h6 className='allFooter_div_title'>Меню</h6>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Главная
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                О компании
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Доставка и оплата
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Партнерам
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Контакты
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                FAQ
+              </a>
+            </div>
+          </div>
+          <div className='allFooter_div_block_two'>
+            <h6 className='allFooter_div_title'>Адрес</h6>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Главная
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                О компании
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Доставка и оплата
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Партнерам
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Контакты
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                FAQ
+              </a>
+            </div>
+          </div>
+          <div className='allFooter_div_block_three'>
+            <h6 className='allFooter_div_title'>Контакты</h6>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Главная
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                О компании
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Доставка и оплата
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Партнерам
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                Контакты
+              </a>
+            </div>
+            <div className='allFooter_div_ahref_div'>
+              <a className='allFooter_div_ahref' href='/'>
+                FAQ
+              </a>
+            </div>
+          </div>
+          <div className='allFooter_div_block_four'>
+            <p className='allFooter_div_block_four_p'>
+              Полный спектр промышленного снабжения. Обращаем ваше внимание на
+              то, что данный Интернет-сайт носит исключительно информационный
+              характер и ни при каких условиях не является публичной офертой,
+              определяемой положениями Статьи 437 Гражданского кодекса
+              Российской Федерации. Для получения подробной информации,
+              стоимости продукции и условий обращайтесь к менеджерам. Вся
+              информация на сайте – собственность интернет-магазина
+              mgb-bearings.ru. Публикация информации с сайта mgb-bearings.ru без
+              разрешения запрещена. Все права защищены. Вы принимаете условия
+              политики конфиденциальности и пользовательского соглашения каждый
+              раз, когда оставляете свои данные в любой форме обратной связи на
+              сайте mgb-bearings.ru.
+            </p>
+          </div>
+        </div>
+        <div className='allFooter_address'>
+          <h1 className='allFooter_address_h1'>
+            © 2023 Подшипники оптом и в розницу.
+          </h1>
+          <div className='allFooter_address_div'>
+            <a href=''>
+              <p className='allFooter_address_div_p'>Разработано WOW</p>
+            </a>
+          </div>
+          <div className='allFooter_address_img_div'>
+            <img
+              className='allFooter_address_img_div_img'
+              src='./img/visa10.svg'
+              alt=''
+            />
+            <img
+              className='allFooter_address_img_div_img'
+              src='./img/visa10.svg'
+              alt=''
+            />
+            <img
+              className='allFooter_address_img_div_img'
+              src='./img/visa10.svg'
+              alt=''
+            />
+            <img
+              className='allFooter_address_img_div_img'
+              src='./img/visa10.svg'
+              alt=''
+            />
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
