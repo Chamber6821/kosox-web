38bb0b76a40f974c37f3756828c93a4802e39a83
Chamber6821 <vania.osinin@yandex.ru>
2024-03-18 13:24:17 +0300

    refactor: move header to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './components/Header'
 import About from './pages/About'
 
 export default function () {
-  return <About />
+  return (
+    <>
+      <Header />
+      <About />
+    </>
+  )
 }
