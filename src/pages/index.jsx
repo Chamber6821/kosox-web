70870d0c61bdcb2a6bf7f6babd95fc95fe70809b
Chamber6821 <vania.osinin@yandex.ru>
2024-03-18 13:39:11 +0300

    feat(pages): add index.jsx with import all pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,23 @@
+import About from './About'
+import AboutManufacturer from './AboutManufacturer'
+import Catalog from './Catalog'
+import Category from './Category'
+import Contacts from './Contacts'
+import DeliveryAndPayment from './DeliveryAndPayment'
+import Faq from './Faq'
+import Home from './Home'
+import Manufacturers from './Manufacturers'
+import Product from './Product'
+
+export {
+  About,
+  AboutManufacturer,
+  Catalog,
+  Category,
+  Contacts,
+  DeliveryAndPayment,
+  Faq,
+  Home,
+  Manufacturers,
+  Product
+}
