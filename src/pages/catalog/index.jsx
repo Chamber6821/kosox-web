b7b0b5a84815e57d9a0f3d86da7b856764f33970
Chamber6821 <vania.osinin@yandex.ru>
2024-05-01 12:33:16 +0300

    feat(catalog): add category name to title

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import Card from './Card'
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 /**
  * @typedef {Object} props
@@ -11,66 +11,73 @@ import Card from './Card'
  * @param {props} props
  * @returns
  */
-export default function Catalog ({ api, params: { superCategory } }) {
-  const [{ categories = [] }, setContent] = useState({})
-  useEffect(() => {
-    ;(async () => {
-      const superCategories = await api.superCategories()
-      const categories = superCategory
-        ? await (await superCategories.withId(superCategory)).categories()
-        : superCategories
-      setContent({
-        categories: await Promise.all(
-          (
-            await categories.array()
-          ).map(async x => ({
-            id: await x.id(),
-            name: await x.name(),
-            icon: await x.icon()
-          }))
-        )
-      })
-    })()
-  }, [superCategory])
-  return (
-    <main>
-      <div
-        style={{
-          backgroundImage:
-            'url("./img/6ba632040d142d29a5ebe2411f406f96 — копия.jpeg")'
-        }}
-        className='header_main'
-      >
-        <div className='header_main_bg' />
-        <div className='header_main_flex'>
-          <h1>Каталог</h1>
-        </div>
-      </div>
-      <div className='kotalog'>
-        <div className='kotalog_flex'>
-          {categories.map(x => (
-            <Card
-              key={x.id}
-              id={x.id}
-              title={x.name}
-              backgroundImage={x.icon}
-              page={superCategory ? `/category/${x.id}` : `/catalog/${x.id}`}
-            />
-          ))}
-        </div>
-      </div>
-      <div
-        style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-        className='contact_form'
-      >
-        <div className='contact_form_flex'>
-          <h2>Оставьте вашу заявку</h2>
-          <input placeholder='Имя' type='text' name='name' />
-          <input placeholder='Имя' type='text' name='surname' />
-          <input placeholder='Имя' type='text' name='phone' />
-          <button>Отправить</button>
-        </div>
-      </div>
-    </main>
-  )
+export default function Catalog({ api, params: { superCategory } }) {
+	const [{ categories = [], categoryName = "" }, setContent] = useState({});
+	useEffect(() => {
+		(async () => {
+			const superCategories = await api.superCategories();
+			const scEntity = await superCategories.withId(superCategory);
+			const categories = superCategory
+				? await scEntity.categories()
+				: superCategories;
+			console.log(scEntity);
+			setContent({
+				categoryName: superCategory && (await scEntity.name()),
+				categories: await Promise.all(
+					(await categories.array()).map(async (x) => ({
+						id: await x.id(),
+						name: await x.name(),
+						icon: await x.icon(),
+					})),
+				),
+			});
+		})();
+	}, [api, superCategory]);
+	return (
+		<main>
+			<div
+				style={{
+					backgroundImage:
+						'url("./img/6ba632040d142d29a5ebe2411f406f96 — копия.jpeg")',
+				}}
+				className="header_main"
+			>
+				<div className="header_main_bg" />
+				<div className="header_main_flex">
+					<h1>Каталог</h1>
+					{superCategory && (
+						<>
+							<h1>/</h1>
+							<h1>{categoryName}</h1>
+						</>
+					)}
+				</div>
+			</div>
+			<div className="kotalog">
+				<div className="kotalog_flex">
+					{categories.map((x) => (
+						<Card
+							key={x.id}
+							id={x.id}
+							title={x.name}
+							backgroundImage={x.icon}
+							page={superCategory ? `/category/${x.id}` : `/catalog/${x.id}`}
+						/>
+					))}
+				</div>
+			</div>
+			<div
+				style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+				className="contact_form"
+			>
+				<div className="contact_form_flex">
+					<h2>Оставьте вашу заявку</h2>
+					<input placeholder="Имя" type="text" name="name" />
+					<input placeholder="Имя" type="text" name="surname" />
+					<input placeholder="Имя" type="text" name="phone" />
+					<button>Отправить</button>
+				</div>
+			</div>
+		</main>
+	);
 }
