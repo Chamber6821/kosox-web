754294447d5e8bf010341238fdac6d84a90b52f6
Chamber6821 <vania.osinin@yandex.ru>
2024-05-23 23:48:24 +0300

    style(pages/AboutManufacturer): auto reformat

--- src/pages/AboutManufacturer.jsx
+++ src/pages/AboutManufacturer.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'wouter'
+import { useEffect, useState } from "react";
+import { Link } from "wouter";
+import Form from "../components/Form";
 
 /**
  * @typedef {Object} props
@@ -11,65 +12,64 @@ import { Link } from 'wouter'
  * @param {props} props
  * @returns
  */
-export default function ({ params: { brand }, api }) {
-  const [{ name, description, categories = [] }, setBrand] = useState({})
+export default function({ params: { brand }, api }) {
+  const [{ name, description, categories = [] }, setBrand] = useState({});
   useEffect(() => {
-    ;(async () => {
-      const brandEntity = await (await api.brands()).withId(brand)
+    (async () => {
+      const brandEntity = await (await api.brands()).withId(brand);
       setBrand({
         name: await brandEntity.name(),
         description: await brandEntity.description(),
         categories: await Promise.all(
-          (
-            await brandEntity.categories()
-          ).map(async x => ({
+          (await brandEntity.categories()).map(async (x) => ({
             id: await x.id(),
             name: await x.name(),
-            icon: await x.icon()
-          }))
-        )
-      })
-    })()
-  }, [brand])
+            icon: await x.icon(),
+          })),
+        ),
+      });
+    })();
+  }, [brand]);
   return (
-    <main className='brendabout'>
-      <div className='brendabout_bgimg'>
-        <img src='./img/Rectangle 94.png' alt='' />
+    <main className="brendabout">
+      <div className="brendabout_bgimg">
+        <img src="./img/Rectangle 94.png" alt="" />
       </div>
-      <div className='brendabout_main_flex'>
-        <div className='main_left'>
+      <div className="brendabout_main_flex">
+        <div className="main_left">
           <h1>{name}</h1>
         </div>
-        <div className='brendabout_title'>
+        <div className="brendabout_title">
           <p>{description}</p>
         </div>
-        <div className='main_left'>
+        <div className="main_left">
           <h6>Товары {name}</h6>
         </div>
-        <div className='brendabout_cards'>
-          {categories.map(x => (
+        <div className="brendabout_cards">
+          {categories.map((x) => (
             <Link to={`/category/${x.id}`}>
               <div
                 style={{
-                  backgroundImage: `url("/img/6ba632040d142d29a5ebe2411f406f96 — копия.jpeg")`
+                  backgroundImage: `url("/img/6ba632040d142d29a5ebe2411f406f96 — копия.jpeg")`,
                 }}
-                className='brendabout_card'
+                className="brendabout_card"
               >
-                <div className='brendabout_card_bg' />
+                <div className="brendabout_card_bg" />
                 <h2>{x.name}</h2>
                 <img
                   style={{ maxWidth: 250, maxHeight: 200 }}
                   src={x.icon}
-                  alt=''
+                  alt=""
                 />
               </div>
             </Link>
           ))}
         </div>
-        <div className='brendabout_btn'>
-          <a href=''>Заказать товар {name}</a>
+        <Form api={api} />
+        <div className="brendabout_btn">
+          <a href="">Заказать товар {name}</a>
         </div>
       </div>
     </main>
-  )
+  );
 }
