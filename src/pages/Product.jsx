207a90adce63e66a5bb07108d2f44d33a6d56d31
Chamber6821 <vania.osinin@yandex.ru>
2024-05-25 14:33:11 +0300

    style(Product): auto format

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,95 +3,95 @@
  * @property {ReturnType<typeof import('../api/Api').default>} api
  */
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 /**
  *
  * @param {props} props
  * @returns
  */
-export default function ({ api, params: { id } }) {
-  const [tab, setTab] = useState('description')
-  const [{ product = {}, brand = {} }, setContent] = useState({})
+export default function({ api, params: { id } }) {
+  const [tab, setTab] = useState("description");
+  const [{ product = {}, brand = {} }, setContent] = useState({});
 
   useEffect(() => {
-    ;(async () => {
-      const product = await (await api.products()).withId(id)
-      const brand = await product.brand()
+    (async () => {
+      const product = await (await api.products()).withId(id);
+      const brand = await product.brand();
       setContent({
         product: {
           icon: await product.icon(),
           name: await product.name(),
           description: await product.description(),
           parameters: await Promise.all(
-            (
-              await product.parameters()
-            ).map(async x => [await x.key(), await x.value()])
-          )
+            (await product.parameters()).map(async (x) => [
+              await x.key(),
+              await x.value(),
+            ]),
+          ),
         },
         brand: {
           icon: await brand.icon(),
-          name: await brand.name()
-        }
-      })
-    })()
-  }, [id])
+          name: await brand.name(),
+        },
+      });
+    })();
+  }, [id]);
   return (
     <main>
-      <div className='product_page content'>
-        <div className='product_page_about '>
+      <div className="product_page content">
+        <div className="product_page_about ">
           <div
-            className='product_page_about_img'
+            className="product_page_about_img"
             style={{ backgroundImage: `url(${product.icon})` }}
           >
-            <div className='product_page_about_img_bg' />
+            <div className="product_page_about_img_bg" />
           </div>
-          <div className='product_page_about_title'>
+          <div className="product_page_about_title">
             <h1>{product.name}</h1>
-            <div className='product_page_about_title_fenom'>
+            <div className="product_page_about_title_fenom">
               <h3>Производитель:</h3>
               {brand.icon ? <img src={brand.icon} /> : <h3>{brand.name}</h3>}
             </div>
-            <a href=''>Заказать</a>
+            <a href="">Заказать</a>
           </div>
         </div>
       </div>
-      <div className='product_page_description'>
-        <div className='product_page_description_menu'>
+      <div className="product_page_description">
+        <div className="product_page_description_menu">
           <div
-            onClick={() => setTab('description')}
+            onClick={() => setTab("description")}
             className={
-              { description: 'product_page_description_menu_title' }[tab] ||
-              'product_page_description_menu_title2'
+              { description: "product_page_description_menu_title" }[tab] ||
+              "product_page_description_menu_title2"
             }
           >
-            <img className='img_texteditor' src='./img/texteditor.svg' alt='' />
-            <h2 className='h2_texteditor'>Описание</h2>
+            <img className="img_texteditor" src="./img/texteditor.svg" alt="" />
+            <h2 className="h2_texteditor">Описание</h2>
           </div>
           <div
-            onClick={() => setTab('parameters')}
+            onClick={() => setTab("parameters")}
             className={
-              { parameters: 'product_page_description_menu_title' }[tab] ||
-              'product_page_description_menu_title2'
+              { parameters: "product_page_description_menu_title" }[tab] ||
+              "product_page_description_menu_title2"
             }
           >
-            <img className='img_searchbox' src='./img/searchbox.svg' alt='' />
-            <h2 className='h2_searchbox'>Характеристики</h2>
+            <img className="img_searchbox" src="./img/searchbox.svg" alt="" />
+            <h2 className="h2_searchbox">Характеристики</h2>
           </div>
         </div>
         <div
-          className={`content ${
-            { description: 'produce_page_description_text' }[tab] ||
-            'produce_page_description_text2'
-          }`}
+          className={`content ${{ description: "produce_page_description_text" }[tab] ||
+            "produce_page_description_text2"
+            }`}
         >
           <p>{product.description}</p>
-          <a href=''>Заказать данный товар</a>
+          <a href="">Заказать данный товар</a>
         </div>
         <div
           className={
-            { parameters: 'produce_page_description_text' }[tab] ||
-            'produce_page_description_text2'
+            { parameters: "produce_page_description_text" }[tab] ||
+            "produce_page_description_text2"
           }
         >
           <table>
@@ -104,9 +104,9 @@ export default function ({ api, params: { id } }) {
               ))}
             </tbody>
           </table>
-          <a href=''>Заказать данный товар</a>
+          <a href="">Заказать данный товар</a>
         </div>
       </div>
     </main>
-  )
+  );
 }
