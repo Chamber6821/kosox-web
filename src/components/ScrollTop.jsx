0c2a56fe8c80477d7e6bfd1a01b871afc2223e6a
Chamber6821 <vania.osinin@yandex.ru>
2024-06-12 15:46:33 +0300

    feat(ScrollTop): add URL query to dependencies

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
-import { useLocation } from 'wouter'
+import { useLocation, useSearch } from 'wouter'
 
 export default function ScrollTop () {
   const location = useLocation()
+  const search = useSearch()
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [location])
+  }, [location, search])
   return <></>
 }
